9418d756de3658aa74d26e80578e79d11c0af3da
zorannkrstic <zorankrstic81@gmail.com>
2023-06-17 04:47:45 +0200

    header style

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import logo from "../../assets/images/logo.png";
 import Navbar from "./Navbar";
+import "./header.css";
 
 const Header: React.FC = () => {
   const navigate = useNavigate();
@@ -12,7 +13,7 @@ const Header: React.FC = () => {
   };
 
   return (
-    <header className=" p-0 m-0 navbar navbar-dark bg-dark">
+    <header className=" p-0 m-0 navbar navbar-dark bg-dark sticky header-background">
       <div className="d-flex flex-row">
         <a className="navbar-brand p-0 m-0" href="/">
           <img src={logo} alt="Logo" onClick={handleLogoClick} />
--- src/components/header/Navbar.tsx
+++ src/components/header/Navbar.tsx
@@ -1,38 +1,33 @@
 import React from "react";
+import "./header.css";
 
 const Navbar: React.FC = () => {
   return (
-    <ul
-      className="navbar-expand-lg navbar-nav d-flex flex-row "
-      style={{
-        paddingTop: "16px",
-        paddingRight: "0px",
-        paddingBottom: "16px",
-        paddingLeft: "53px",
-      }}
-    >
+    <ul className="navbar-expand-lg navbar-nav d-flex flex-row navbar-padding ">
       <li className="nav-item px-3">
-        <a className="nav-link" href="/rent">
+        <a className="nav-link navbar-link" href="/rent">
           Rent
+          <span className="dropdown-indicator"></span>
         </a>
       </li>
       <li className="nav-item px-3">
-        <a className="nav-link" href="/sales">
+        <a className="nav-link navbar-link" href="/sales">
           Sales
+          <span className="dropdown-indicator"></span>
         </a>
       </li>
       <li className="nav-item px-3">
-        <a className="nav-link" href="/team">
+        <a className="nav-link navbar-link" href="/team">
           Team
         </a>
       </li>
       <li className="nav-item px-3">
-        <a className="nav-link" href="/blog">
+        <a className="nav-link navbar-link" href="/blog">
           Blog
         </a>
       </li>
       <li className="nav-item px-3">
-        <a className="nav-link" href="/contact">
+        <a className="nav-link navbar-link" href="/contact">
           Contact
         </a>
       </li>
