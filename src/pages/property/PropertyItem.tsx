8bc4bb37959ba4973714bc7a855004aa7f989c26
zorannkrstic <zorankrstic81@gmail.com>
2023-08-01 12:13:24 +0200

    property card style

--- src/pages/property/PropertyItem.tsx
+++ src/pages/property/PropertyItem.tsx
@@ -8,6 +8,10 @@ import { useGetUser } from "../../hook/useGetUser";
 import PropertyCarousel from "./PropertyCaorusel";
 import Header from "../../components/header/Header";
 import Footer from "../../components/footer/Footer";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPhone } from "@fortawesome/free-solid-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faWhatsapp } from "@fortawesome/free-brands-svg-icons";
 
 const GET_PROPERTY = gql`
   query GetProperty($num: Int!) {
@@ -47,44 +51,63 @@ const PropertyItem: FC = () => {
       <Header />
       <Container>
         <Row className="property-details">
-          <div className="col-6 property-carousel-container">
+          <div className="col-12 col-lg-8  property-carousel-container">
             <PropertyCarousel property={properties} />
           </div>
-          <div className="col-6  user-property-card ">
-            <h5>{`REF NO ${properties.num}`}</h5>
-            <h2>
+          <div className="col-12 col-lg-4 mt-3 mt-lg-0 user-property-card ">
+            <h5 className="property-number">{`REF NO ${properties.num}`}</h5>
+            <h2 className="property-details-title">
               {`${
                 properties.property?.charAt(0).toUpperCase() +
                 properties.property?.slice(1)
               } in ${properties.place}`}
             </h2>
+            <hr />
             <Row>
-              <div>
-                <h5>SIZE AVAILABLE</h5>
-                <h2>{`${properties.square} m\u00B2`}</h2>
+              <div className="col-6">
+                <h5 className="property-size-price-title">SIZE AVAILABLE</h5>
+                <h2 className="property-size-price">{`${properties.square} m\u00B2`}</h2>
               </div>
-              <div>
-                <h5>PRICE</h5>
-                <h2>{`€${properties.price}/mo`}</h2>
+              <div className="col-6">
+                <h5 className="property-size-price-title">PRICE</h5>
+                <h2 className="property-size-price">{`€${properties.price}/mo`}</h2>
               </div>
             </Row>
-            <div>
+            <hr />
+            <div className="agent-card-details">
               <h5>Specialist information</h5>
-              <div>
+              <div className="d-flex">
                 <div className="agent-photo">
                   <img src={user.img} alt={user.user} />
                 </div>
-                <div>
+                <div className="agent-name-language">
                   <h3>{user.name}</h3>
-                  <h4>{user.language}</h4>
+                  <h4>
+                    10 Nikole Tesle Boulevard, New Belgrade, 5th floor,
+                    Apartment 23
+                  </h4>
                 </div>
               </div>
-              <p>
-                <Link to={`tel:${user.phone}`}>{user.phone}</Link>
+            </div>
+            <div className="agent-phone-email-container">
+              <p className="agent-phone-email">
+                <FontAwesomeIcon icon={faPhone} className="phone-email-icon" />{" "}
+                <Link to={`tel:${user.phone}`}>Call {user.name}</Link>
               </p>
-              <p>
-                <Link to={`mailto:${user.email}`}>{user.email}</Link>
+              <p className="agent-phone-email">
+                <span className="phone-email-icon">
+                  <FontAwesomeIcon icon={faEnvelope} className="icon" />{" "}
+                </span>
+                <Link to={`mailto:${user.email}`}>Email {user.name}</Link>
               </p>
+              <p className="agent-phone-email">
+                <FontAwesomeIcon
+                  icon={faWhatsapp}
+                  className="phone-email-icon"
+                />{" "}
+                <Link to={`tel:${user.phone}`}>Whatsapp {user.name}</Link>
+              </p>
+              <p>{user.language}</p>
             </div>
           </div>
         </Row>
