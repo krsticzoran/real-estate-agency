a322cac265a1ff93e3a7275cb050b0605a064b99
zorannkrstic <zorankrstic81@gmail.com>
2023-08-02 20:43:05 +0200

    search

--- server/schema/realestate.ts
+++ server/schema/realestate.ts
@@ -19,6 +19,9 @@ const RealEstateType = new GraphQLObjectType({
     time: { type: GraphQLInt },
     img: { type: GraphQLString },
     specialist: { type: GraphQLString },
+    img1: { type: GraphQLString },
+    img2: { type: GraphQLString },
+    img3: { type: GraphQLString },
   },
 });
 
@@ -43,6 +46,41 @@ function schemaRealEstate(database: Db): GraphQLSchema {
         },
         async resolve(parentValue, args) {
           const properties = await collection.find(args).toArray();
+          return properties;
+        },
+      },
+      search: {
+        type: new GraphQLList(RealEstateType),
+        args: {
+          property: { type: GraphQLString },
+          sale: { type: GraphQLString },
+          place: { type: GraphQLString },
+          minPrice: { type: GraphQLInt },
+          maxPrice: { type: GraphQLInt },
+        },
+        async resolve(parentValue, args) {
+          const { property, sale, place, minPrice, maxPrice } = args;
+
+          // Prepare the search filter based on provided arguments
+          const searchFilter: any = {};
+          if (property && property.toLowerCase() !== "all") {
+            searchFilter.property = property;
+          }
+          if (place && place.toLowerCase() !== "all") {
+            searchFilter.place = place;
+          }
+          if (sale) {
+            searchFilter.sale = sale;
+          }
+          if (minPrice !== undefined && maxPrice !== undefined) {
+            searchFilter.price = { $gte: minPrice, $lte: maxPrice };
+          } else if (minPrice !== undefined) {
+            searchFilter.price = { $gte: minPrice };
+          } else if (maxPrice !== undefined) {
+            searchFilter.price = { $lte: maxPrice };
+          }
+
+          const properties = await collection.find(searchFilter).toArray();
           return properties;
         },
       },
