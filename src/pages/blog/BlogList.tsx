d72a5b889eccbe17d4b35d2567ab820f87da1d47
zorannkrstic <zorankrstic81@gmail.com>
2023-08-08 22:39:03 +0200

    blog top style

--- src/pages/blog/BlogList.tsx
+++ src/pages/blog/BlogList.tsx
@@ -1,14 +1,19 @@
 import { FC } from "react";
-
+import "./blog.css";
 import { useQuery } from "@apollo/client";
 import { gql } from "graphql-tag";
 import React from "react";
+import Header from "../../components/header/Header";
+import { Container, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const GET_BLOGS = gql`
   query {
     blogList {
-      content
       title
+      author
+      property
+      num
     }
   }
 `;
@@ -19,21 +24,33 @@ const BlogList: FC = () => {
   });
 
   const dataBlog = data?.blogList ?? [];
-
-  const divStyle = {
-    height: "100vh",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-  };
+  console.log(dataBlog[0]?.title);
 
   return (
-    <div style={divStyle}>
-      <p>
-        This page is currently under construction. Please check back later for
-        updates.
-      </p>
-    </div>
+    <>
+      <Header />
+      <div className="blog-cover-img-container">
+        <img
+          src="/img/blog/cover.jpeg"
+          alt="cover"
+          className="blog-cover-img"
+        />
+        <div className="img-overlay" />
+        <div className="blog-overlay-text">
+          <Container>
+            <Row>
+              <div className="col-md-6">
+                <h5>{dataBlog[0]?.author}</h5>
+                <h1>{dataBlog[0]?.title}</h1>
+                <Link to={`/blog/${dataBlog[0]?.title.replace(/\s+/g, "-")}`}>
+                  Read Article
+                </Link>
+              </div>
+            </Row>
+          </Container>
+        </div>
+      </div>
+    </>
   );
 };
 
