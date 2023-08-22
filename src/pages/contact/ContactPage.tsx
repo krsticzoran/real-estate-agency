5c0912f1b9d26fcb67f9912789907367740a8cb6
zorannkrstic <zorankrstic81@gmail.com>
2023-08-22 12:37:50 +0200

    contact style

--- src/pages/contact/ContactPage.tsx
+++ src/pages/contact/ContactPage.tsx
@@ -1,7 +1,7 @@
-import React, { FC, useEffect, useRef, useState } from "react";
+import React, { FC, useEffect, useState } from "react";
 import Header from "../../components/header/Header";
 import Footer from "../../components/footer/Footer";
-import { Container } from "react-bootstrap";
+import { Container, Row } from "react-bootstrap";
 import "./contact.css";
 import emailjs from "emailjs-com";
 
@@ -74,64 +74,73 @@ const ContactPage: FC = () => {
 
   return (
     <>
+      <Header />
       <Container>
-        <div className="col-12 col-sm-6 contact-img-box">
-          <img src="/img/contact/contact.jpeg" alt="img" />
-        </div>
-        <div className="col-12 col-sm-6">
-          <h1>Contact Real Estate Belgrade</h1>
-          <p>
-            Hey, we’re here to help. Get in touch with us and <br></br>
-            we’ll help in whatever way we can
-          </p>
-          <form onSubmit={handleSubmit}>
-            <input
-              type="text"
-              name="name"
-              placeholder="Name"
-              value={formData.name}
-              onChange={(e) =>
-                setFormData({ ...formData, name: e.target.value })
-              }
-              required
-            />
+        <Row>
+          <div className="col-12 col-lg-6 contact-img-box">
+            <img src="/img/contact/contact.jpeg" alt="img" />
+          </div>
+          <div className="col-12 col-lg-6 ">
+            <div className="contact-form">
+              <h1>Contact Real Estate Belgrade</h1>
+              <p>
+                Hey, we’re here to help. Get in touch with us and <br></br>
+                we’ll help in whatever way we can
+              </p>
+              <form onSubmit={handleSubmit} className="contact-form-box">
+                <label>NAME</label>
+                <input
+                  type="text"
+                  name="name"
+                  placeholder="Name"
+                  value={formData.name}
+                  onChange={(e) =>
+                    setFormData({ ...formData, name: e.target.value })
+                  }
+                  required
+                />
+                <label>EMAIL</label>
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Email"
+                  value={formData.email}
+                  onChange={(e) =>
+                    setFormData({ ...formData, email: e.target.value })
+                  }
+                  required
+                />
+                <label>PHONE NUMBER</label>
+                <input
+                  type="number"
+                  name="number"
+                  placeholder="Number"
+                  value={formData.number}
+                  onChange={(e) =>
+                    setFormData({ ...formData, number: e.target.value })
+                  }
+                  required
+                />
+                <label>MESSAGE</label>
+                <input
+                  type="text"
+                  name="message"
+                  placeholder="Message"
+                  value={formData.message}
+                  onChange={(e) =>
+                    setFormData({ ...formData, message: e.target.value })
+                  }
+                  required
+                />
 
-            <input
-              type="email"
-              name="email"
-              placeholder="email"
-              value={formData.email}
-              onChange={(e) =>
-                setFormData({ ...formData, email: e.target.value })
-              }
-              required
-            />
-            <input
-              type="number"
-              name="number"
-              placeholder="number"
-              value={formData.number}
-              onChange={(e) =>
-                setFormData({ ...formData, number: e.target.value })
-              }
-              required
-            />
-            <input
-              type="text"
-              name="message"
-              placeholder="message"
-              value={formData.message}
-              onChange={(e) =>
-                setFormData({ ...formData, message: e.target.value })
-              }
-              required
-            />
-
-            <button type="submit">Submit</button>
-            <p>{successMessage}</p>
-          </form>
-        </div>
+                <button type="submit">Submit</button>
+                <p>{successMessage}</p>
+              </form>
+            </div>
+          </div>
+        </Row>
       </Container>
+      <Footer />
     </>
   );
 };
