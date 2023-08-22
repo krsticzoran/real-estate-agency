de4858aec711047ed3a382dbea98cdf7bfbf308a
zorannkrstic <zorankrstic81@gmail.com>
2023-08-22 20:42:31 +0200

    featured

--- src/pages/home/featured/Featured.tsx
+++ src/pages/home/featured/Featured.tsx
@@ -6,8 +6,51 @@ import "./featured.css";
 import p2 from "../../../assets/images/home/p2.jpeg";
 import p6 from "../../../assets/images/home/p6.jpeg";
 import p1 from "../../../assets/images/home/p1.jpeg";
+import { gql, useQuery } from "@apollo/client";
+import { Link } from "react-router-dom";
+import { Property } from "../../../types";
+
+const GET_PROPERTIES = gql`
+  query GetProperties(
+    $property: String!
+    $sale: String!
+    $minPrice: Int!
+    $maxPrice: Int!
+    $place: String!
+  ) {
+    search(
+      property: $property
+      sale: $sale
+      place: $place
+      minPrice: $minPrice
+      maxPrice: $maxPrice
+    ) {
+      property
+      sale
+      num
+      place
+      price
+      square
+      time
+      img
+    }
+  }
+`;
 
 const Featured: FC = () => {
+  const { data } = useQuery(GET_PROPERTIES, {
+    variables: {
+      property: "All",
+      sale: "rent",
+      place: "All",
+      minPrice: 0,
+      maxPrice: 1000000,
+    },
+    context: { clientName: "endpoint2" },
+  });
+
+  const properties = data?.search ?? [];
+
   return (
     <div className="featured">
       <Container>
@@ -18,36 +61,27 @@ const Featured: FC = () => {
           </div>
           <div className="col-12 featured-container">
             <Row>
-              <div className="col-md-4 col-12">
-                <Card className="featured-box mb-1 mt-1">
-                  <Card.Body>
-                    <img src={p6} alt="restaurant" />
-                    <h3 className="featured-h3-title">Restaurants</h3>
-                    <p className="featured-price">€400/mo</p>
-                    <p className="featured-day">1 day ago</p>
-                  </Card.Body>
-                </Card>
-              </div>
-              <div className="col-md-4 col-12">
-                <Card className="featured-box mb-1 mt-1">
-                  <Card.Body>
-                    <img src={p1} alt="restaurant" />
-                    <h3 className="featured-h3-title">Factories</h3>
-                    <p className="featured-price">€3000/mo</p>
-                    <p className="featured-day">10 days ago</p>
-                  </Card.Body>
-                </Card>
-              </div>
-              <div className="col-md-4 col-12">
-                <Card className="featured-box mb-1 mt-1">
-                  <Card.Body>
-                    <img src={p2} alt="shop" />
-                    <h3 className="featured-h3-title">Shops</h3>
-                    <p className="featured-price">€800/mo</p>
-                    <p className="featured-day">15 days ago</p>
-                  </Card.Body>
-                </Card>
-              </div>
+              {properties
+                .slice(0, 3)
+                .map((property: Property, index: number) => (
+                  <div className="col-md-4 col-12">
+                    <Link to={`/property/${property.num}`}>
+                      <Card className="featured-box mb-1 mt-1">
+                        <Card.Body>
+                          <img src={property.img} alt={property.img} />
+                          <h3 className="featured-h3-title">
+                            {`${
+                              property.property.charAt(0).toUpperCase() +
+                              property.property.slice(1)
+                            } `}
+                          </h3>
+                          <p className="featured-price">{`€${property.price}/mo`}</p>
+                          <p className="featured-day">{`${property.time} days ago`}</p>
+                        </Card.Body>
+                      </Card>
+                    </Link>
+                  </div>
+                ))}
             </Row>
           </div>
         </Row>
