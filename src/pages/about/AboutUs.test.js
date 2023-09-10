c301f5a2580b663c758234c07443244f8ef598a8
zorannkrstic <zorankrstic81@gmail.com>
2023-09-10 09:21:57 +0200

    test about us

--- src/pages/about/AboutUs.test.js
+++ src/pages/about/AboutUs.test.js
@@ -16,20 +16,15 @@ describe("AboutUs Component", () => {
   it("should render the About Us page content", () => {
     render(<AboutUs />);
 
-    // Assert that specific text content is present in the component
     expect(screen.getByText("About us")).toBeInTheDocument();
     expect(
       screen.getByText("Experienced Team, Personal Approach")
     ).toBeInTheDocument();
-    // Add more assertions for other content you want to test
   });
 
   it("should display an image", () => {
     render(<AboutUs />);
 
-    // Assert that an image with the alt text 'about us' is present
     expect(screen.getByAltText("about us")).toBeInTheDocument();
   });
-
-  // Add more test cases as needed to cover other aspects of your component
 });
