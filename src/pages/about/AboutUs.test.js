e8c8e445eec19114ae1d8301da346e1d17361d5f
zorannkrstic <zorankrstic81@gmail.com>
2023-09-25 21:13:42 +0200

    about us test

--- src/pages/about/AboutUs.test.js
+++ src/pages/about/AboutUs.test.js
@@ -2,16 +2,6 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import AboutUs from "./AboutUs";
 
-// Mock the Header component
-jest.mock("../../components/header/Header", () => {
-  return () => <div data-testid="mocked-header">Mocked Header</div>;
-});
-
-// Mock the Footer component
-jest.mock("../../components/footer/Footer", () => {
-  return () => <div data-testid="mocked-footer">Mocked Footer</div>;
-});
-
 describe("AboutUs Component", () => {
   it("should render the About Us page content", () => {
     render(<AboutUs />);
