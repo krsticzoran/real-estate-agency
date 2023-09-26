3b230cd440fe9a5a1b39269b4cdad06855a4bc21
zorannkrstic <zorankrstic81@gmail.com>
2023-09-26 12:34:18 +0200

    lazy

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,41 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
-import HomePage from "./pages/home/HomePage";
-import ContactPage from "./pages/contact/ContactPage";
 import "./App.css";
-import Team from "./pages/team/Team";
-import Member from "./pages/team/Member";
-import PropertyList from "./pages/property/PropertyList";
-import PropertyItem from "./pages/property/PropertyItem";
-import Search from "./pages/search/Search";
-import BlogList from "./pages/blog/BlogList";
-import BlogText from "./pages/blog/BlogText";
-import AboutUs from "./pages/about/AboutUs";
 import ScrollToTop from "./components/ScrollToTop/ScrolllToTop";
 import Layout from "./components/layout/Layout";
+
+// Lazy-loaded components
+const HomePage = lazy(() => import("./pages/home/HomePage"));
+const ContactPage = lazy(() => import("./pages/contact/ContactPage"));
+const Team = lazy(() => import("./pages/team/Team"));
+const Member = lazy(() => import("./pages/team/Member"));
+const PropertyList = lazy(() => import("./pages/property/PropertyList"));
+const PropertyItem = lazy(() => import("./pages/property/PropertyItem"));
+const Search = lazy(() => import("./pages/search/Search"));
+const BlogList = lazy(() => import("./pages/blog/BlogList"));
+const BlogText = lazy(() => import("./pages/blog/BlogText"));
+const AboutUs = lazy(() => import("./pages/about/AboutUs"));
 
 function App() {
   return (
     <BrowserRouter>
       <Layout>
         <ScrollToTop />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/team" element={<Team />} />
-          <Route path="/team/:memberName" element={<Member />} />
-          <Route path="/property/:item" element={<PropertyItem />} />
-          <Route path="/:sale/:rentproperty" element={<PropertyList />} />
-          <Route path="/search" element={<Search />} />
-          <Route path="/blog" element={<BlogList />} />
-          <Route path="/blog/:title" element={<BlogText />} />
-          <Route path="/contact" element={<ContactPage />} />
-          <Route path="/about-us" element={<AboutUs />} />
-          <Route path="/*" element={<HomePage />} />
-        </Routes>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/team" element={<Team />} />
+            <Route path="/team/:memberName" element={<Member />} />
+            <Route path="/property/:item" element={<PropertyItem />} />
+            <Route path="/:sale/:rentproperty" element={<PropertyList />} />
+            <Route path="/search" element={<Search />} />
+            <Route path="/blog" element={<BlogList />} />
+            <Route path="/blog/:title" element={<BlogText />} />
+            <Route path="/contact" element={<ContactPage />} />
+            <Route path="/about-us" element={<AboutUs />} />
+            <Route path="/*" element={<HomePage />} />
+          </Routes>
+        </Suspense>
       </Layout>
     </BrowserRouter>
   );
