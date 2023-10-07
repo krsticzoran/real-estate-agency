aff381b0a366b318ae971817860e2b130b749416
zorannkrstic <zorankrstic81@gmail.com>
2023-10-07 14:30:44 +0200

    search test

--- src/pages/search/search.test.js
+++ src/pages/search/search.test.js
@@ -3,37 +3,65 @@ import { render, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import Search from "./Search";
 
-// Mock the useLocation hook to return the desired data
+const defaultMockLocation = () => ({
+  state: {
+    data: {
+      search: [],
+    },
+  },
+});
+
 jest.mock("react-router-dom", () => ({
   ...jest.requireActual("react-router-dom"),
-  useLocation: () => ({
-    state: {
-      data: {
-        search: [
-          {
-            img: "img.jpg",
-            num: 1,
-            place: "Novi Beograd",
-            price: 300,
-            property: "offices",
-            sale: "rent",
-            square: 25,
-            time: 2,
-          },
-        ],
-      },
-    },
-  }),
+  useLocation: defaultMockLocation,
 }));
 
-test("Search component", () => {
+beforeEach(() => {
+  jest
+    .spyOn(require("react-router-dom"), "useLocation")
+    .mockImplementation(defaultMockLocation);
+});
+
+test("Search component when data is empty", () => {
   render(
     <MemoryRouter initialEntries={["/search"]}>
       <Search />
     </MemoryRouter>
   );
 
-  // Now, test for the presence of the rendered search results instead
-  const searchResultElement = screen.getByText(/offices in Novi Beograd/i);
+  const noSearchResultsText = screen.getByText("No search results");
+  expect(noSearchResultsText).toBeInTheDocument();
+});
+
+test("Search component when data is not empty", () => {
+  const searchData = {
+    search: [
+      {
+        img: "img.jpg",
+        num: 1,
+        place: "Novi Beograd",
+        price: 300,
+        property: "offices",
+        sale: "rent",
+        square: 25,
+        time: 2,
+      },
+    ],
+  };
+
+  jest.spyOn(require("react-router-dom"), "useLocation").mockReturnValue({
+    state: {
+      data: searchData,
+    },
+  });
+
+  render(
+    <MemoryRouter initialEntries={["/search"]}>
+      <Search />
+    </MemoryRouter>
+  );
+
+  // Test for the presence of data when it's not empty
+  const searchResultElement = screen.getByText("Novi Beograd");
   expect(searchResultElement).toBeInTheDocument();
 });
