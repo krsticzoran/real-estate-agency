d3b568a555f5d155b0d59ee0f41f3dbf6a9743e9
zorannkrstic <zorankrstic81@gmail.com>
2023-10-10 11:15:02 +0200

    member test

---
+++ src/pages/team/member.test.js
@@ -0,0 +1,62 @@
+import React from "react";
+import { screen, render } from "@testing-library/react";
+import Member from "./Member";
+import { MemoryRouter } from "react-router-dom";
+
+jest.mock("@apollo/client", () => {
+  const actualApolloClient = jest.requireActual("@apollo/client");
+
+  return {
+    ...actualApolloClient,
+    useQuery: () => ({
+      data: {
+        staff: [
+          {
+            property: "mock Property",
+            sale: "rent",
+            num: 2,
+            place: "mock place",
+            price: 2000,
+            square: 1000,
+            time: 2,
+            specialist: "mock Specialist",
+            img: "image1.jpg",
+          },
+        ],
+        user: {
+          name: "sample name",
+          language: "sample language",
+          img: "user.jpg",
+          user: "sample user",
+        },
+      },
+    }),
+  };
+});
+
+test("render Member component", () => {
+  render(
+    <MemoryRouter>
+      <Member />
+    </MemoryRouter>
+  );
+
+  expect(
+    screen.getByRole("heading", {
+      name: /sample name/i,
+      level: 3,
+    })
+  ).toBeInTheDocument();
+
+  expect(screen.getByText(/sample language/i)).toBeInTheDocument();
+
+  expect(
+    screen.getByRole("heading", {
+      name: /mock place/i,
+      level: 4,
+    })
+  ).toBeInTheDocument();
+
+  expect(screen.getByText(/2,000/i)).toBeInTheDocument();
+  expect(screen.getByText(/Mock Property in mock place/i)).toBeInTheDocument();
+});
