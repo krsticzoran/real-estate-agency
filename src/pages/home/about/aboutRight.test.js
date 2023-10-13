8f0295027b388495a7c25e4043aee2fc78b5e15d
zorannkrstic <zorankrstic81@gmail.com>
2023-10-13 19:44:21 +0200

    about right test

---
+++ src/pages/home/about/aboutRight.test.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import AboutRight from "./AboutRight";
+
+test("render AboutRight component", () => {
+  render(<AboutRight />);
+
+  expect(
+    screen.getByRole("heading", {
+      name: /Local Knowledge/i,
+      level: 3,
+    })
+  ).toBeInTheDocument();
+
+  expect(
+    screen.getByRole("heading", {
+      name: /Expert Team/i,
+      level: 3,
+    })
+  ).toBeInTheDocument();
+
+  expect(screen.getByText(/who love Belgrade/i)).toBeInTheDocument();
+
+  expect(screen.getByText(/Trusted by many local/i)).toBeInTheDocument();
+
+  expect(screen.getAllByAltText(/benefit/i)).toHaveLength(4);
+});
