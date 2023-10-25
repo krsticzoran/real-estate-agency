551fc828853faa3aa3812158c57b8182b903ce7b
zorannkrstic <zorankrstic81@gmail.com>
2023-10-25 18:33:46 +0200

    social data test

---
+++ src/pages/home/social/socialData.test.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { screen, render } from "@testing-library/react";
+import SocialData from "./SocialData";
+
+test("render social data component", () => {
+  render(<SocialData />);
+
+  expect(screen.getAllByAltText(/testimonial/i)).toHaveLength(4);
+
+  expect(screen.getByText(/Maja Markovic/i)).toBeInTheDocument();
+
+  expect(
+    screen.getByText(/fantastic team of specialist ready/i)
+  ).toBeInTheDocument();
+
+  expect(screen.getByText(/milos markovic/i)).toBeInTheDocument();
+});
