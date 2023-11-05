d3caab5b79baba8762764fbe5c6215ddd52f6e35
zorannkrstic <zorankrstic81@gmail.com>
2023-11-05 12:48:06 +0100

    login basic logic

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -1,8 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container } from "react-bootstrap";
+import "./login.css";
 
 const LoginPage = () => {
-  return <div className="login-container">Login page</div>;
+  const [formData, setFormData] = useState({ user: "", password: "" });
+
+  const handleChange = (event: { target: { name: any; value: any } }) => {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+  };
+
+  const handleSubmitform = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(formData);
+    setFormData({ user: "", password: "" });
+  };
+
+  return (
+    <div className="login-container">
+      <form onSubmit={handleSubmitform}>
+        <label htmlFor="user">
+          User:
+          <input
+            type="text"
+            id="user"
+            name="user"
+            value={formData.user}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="password">
+          Passowrd
+          <input
+            type="text"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+          />
+        </label>
+
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 };
 
 export default LoginPage;
