6f810a02cb61079cfe7536c3ba8d6353d0e6e495
zorannkrstic <zorankrstic81@gmail.com>
2023-11-20 12:03:09 +0100

    switch to dark mode btn

--- src/pages/dashboard/header/Header.tsx
+++ src/pages/dashboard/header/Header.tsx
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import admin from "../../../assets/images/dashboard/admin.jpeg";
+import Form from "react-bootstrap/Form";
+
 const Header = () => {
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleSwitchChange = () => {
+    setIsChecked(!isChecked);
+  };
   return (
     <div className="col-lg-10 col-sm-12 d-flex align-items-center justify-content-end dashboard-header">
+      <Form className="p-5">
+        <Form.Check
+          type="switch"
+          id="custom-switch"
+          label={isChecked ? "Dark" : "Light"}
+          checked={isChecked}
+          onChange={handleSwitchChange}
+        />
+      </Form>
       <div className="dash-header-info">
         <span>Henry Jr.</span>
         <p>Admin</p>
