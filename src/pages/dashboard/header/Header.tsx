242e2df63e7900982bf120102eec0c787367bea4
zorannkrstic <zorankrstic81@gmail.com>
2023-11-22 11:55:38 +0100

    header

--- src/pages/dashboard/header/Header.tsx
+++ src/pages/dashboard/header/Header.tsx
@@ -9,21 +9,24 @@ interface HeaderProps {
 
 const Header: React.FC<HeaderProps> = ({ isChecked, handleSwitchChange }) => {
   return (
-    <div className="col-lg-10 col-sm-12 d-flex align-items-center justify-content-end dashboard-header">
-      <Form className="p-5">
-        <Form.Check
-          type="switch"
-          id="custom-switch"
-          label={isChecked ? "Dark" : "Light"}
-          checked={isChecked}
-          onChange={handleSwitchChange}
-        />
-      </Form>
-      <div className="dash-header-info">
-        <span>Henry Jr.</span>
-        <p>Admin</p>
+    <div className="col-lg-10 col-sm-12 d-flex justify-content-between align-items-center dashboard-header ">
+      <p className="pt-2">Welcome to RE Belgrade dashboard</p>
+      <div className="d-flex align-items-center">
+        <Form className="p-5 ">
+          <Form.Check
+            type="switch"
+            id="custom-switch"
+            label={isChecked ? "Dark" : "Light"}
+            checked={isChecked}
+            onChange={handleSwitchChange}
+          />
+        </Form>
+        <div className="dash-header-info">
+          <span>Henry Jr.</span>
+          <p>Admin</p>
+        </div>
+        <img src={admin} alt="admin" />
       </div>
-      <img src={admin} alt="admin" />
     </div>
   );
 };
