3dec0afa824fe750f83b54c93477f00825029c81
zorannkrstic <zorankrstic81@gmail.com>
2023-11-29 12:50:40 +0100

    progress bar style

--- src/pages/dashboard/progressBar/ProgressBar.tsx
+++ src/pages/dashboard/progressBar/ProgressBar.tsx
@@ -2,6 +2,10 @@ import { gql } from "@apollo/client";
 import { FC } from "react";
 import ProgressBar from "react-bootstrap/ProgressBar";
 import { useQuery } from "@apollo/client";
+import { Row } from "react-bootstrap";
+import "../dashboard.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHouse } from "@fortawesome/free-solid-svg-icons";
 
 const GET_PROPERTIES = gql`
   query GetProperties($sale: String!) {
@@ -38,24 +42,34 @@ const CustomProgressBar: FC = () => {
   ).toFixed(0);
 
   return (
-    <div>
-      <div className="d-flex justify-content-between ">
-        <span>rent</span>
-        <span>sale</span>
-      </div>
-      <ProgressBar>
-        <ProgressBar
-          striped
-          style={{ backgroundColor: "blue" }}
-          now={Number(percentRent)}
-          key={1}
-        />
-        <ProgressBar
-          style={{ backgroundColor: "red" }}
-          now={100 - Number(percentRent)}
-          key={2}
-        />
-      </ProgressBar>
+    <div className="progress-bar-container d-flex justify-content-center align-items-center">
+      <Row className="w-100">
+        <div className="d-flex  ">
+          <div className="col-sm-1 h-100">
+            <FontAwesomeIcon className="progress-bar-icon" icon={faHouse} />
+          </div>
+          <div className="col-sm-11 ">
+            <h4>Total Properties {propertyRent + propertySale}</h4>
+            <ProgressBar>
+              <ProgressBar
+                striped
+                style={{ backgroundColor: "#fff" }}
+                now={Number(percentRent)}
+                key={1}
+              />
+              <ProgressBar
+                style={{ backgroundColor: "#e9ecef" }}
+                now={100 - Number(percentRent)}
+                key={2}
+              />
+            </ProgressBar>
+            <div className="d-flex justify-content-between ">
+              <span>{propertyRent} rent</span>
+              <span>{propertySale} sale</span>
+            </div>
+          </div>
+        </div>
+      </Row>
     </div>
   );
 };
