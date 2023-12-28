10b69f62196c56f6134273a7b5d885ad9aea9fbc
zorannkrstic <zorankrstic81@gmail.com>
2023-12-28 16:44:55 +0100

    handle add property click

--- src/pages/dashboard/menu/Menu.tsx
+++ src/pages/dashboard/menu/Menu.tsx
@@ -2,19 +2,25 @@ import React, { FC } from "react";
 import "./../dashboard.css";
 import logo from "../../../assets/images/header/logo.png";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router";
 
 interface ChildProps {
   logout: () => void;
 }
 
 const Menu: FC<ChildProps> = ({ logout }) => {
+  const navigate = useNavigate();
+  const handleAddPropertyClick = () => {
+    navigate("/dashboard/add-property");
+  };
+
   return (
     <div className="dashboard-menu col-lg-2 col-sm-12">
       <Link to="/" className="">
         <img src={logo} alt="Logo" />
       </Link>
       <button>Dashboard</button>
-      <button>Add Property</button>
+      <button onClick={handleAddPropertyClick}>Add Property</button>
       <button>Blog Post</button>
       <button>Team Member</button>
       <button>Manage Users</button>
