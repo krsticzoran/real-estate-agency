7719ee1a33bd4fcc1e9c80b9810a1067c1d457d3
zorannkrstic <zorankrstic81@gmail.com>
2024-01-12 14:03:46 +0100

    back btn

--- src/pages/dashboard/addPrpperty/addProperty.tsx
+++ src/pages/dashboard/addPrpperty/addProperty.tsx
@@ -6,6 +6,7 @@ import Row from "react-bootstrap/Row";
 import gql from "graphql-tag";
 import { useMutation } from "@apollo/client";
 import axios from "axios";
+import { useNavigate } from "react-router";
 
 interface FormData {
   sale: string;
@@ -70,11 +71,17 @@ const ADD_PROPERTY = gql`
 `;
 
 const AddProperty: FC = () => {
+  const navigate = useNavigate();
+
   const [formData, setFormData] = useState<FormData>(defaultFormData);
   const file1Ref = useRef<HTMLInputElement>(null);
   const file2Ref = useRef<HTMLInputElement>(null);
   const file3Ref = useRef<HTMLInputElement>(null);
   const file4Ref = useRef<HTMLInputElement>(null);
+
+  const handleReturnBackClick = () => {
+    navigate("/dashboard/");
+  };
 
   const [executeMutation] = useMutation(ADD_PROPERTY, {
     context: { clientName: "endpoint2" },
@@ -173,116 +180,120 @@ const AddProperty: FC = () => {
   };
 
   return (
-    <Form onSubmit={handleSubmit}>
-      <Row>
-        <Col>
-          <Form.Select
-            aria-label="Default select example"
-            name="sale"
-            value={formData.sale}
-            onChange={handleInputChange}
-          >
-            <option value="sale">Sale</option>
-            <option value="rent">Rent</option>
-          </Form.Select>
-        </Col>
-        <Col>
-          <Form.Select
-            aria-label="Default select example"
-            name="property"
-            value={formData.property}
-            onChange={handleInputChange}
-          >
-            <option value="offices">Office</option>
-            <option value="shops">Shop</option>
-            <option value="warehouses">Warehouse</option>
-            <option value="catering">Catering</option>
-          </Form.Select>
-        </Col>
-        <Col>
-          <Form.Select
-            aria-label="Default select example"
-            name="place"
-            value={formData.place}
-            onChange={handleInputChange}
-          >
-            <option value="Zvezdara">Zvezdara</option>
-            <option value="Novi Beograd">Novi Beograd</option>
-            <option value="Savski venac">Savski venac</option>
-            <option value="Palilula">Palilula</option>
-            <option value="Stari grad">Stari grad</option>
-            <option value="Mladenovac">Mladenovac</option>
-          </Form.Select>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <Form.Label htmlFor="inputPrice">Price</Form.Label>
-          <Form.Control
-            type="text"
-            id="inputPrice"
-            name="price"
-            value={formData.price}
-            onChange={handleInputChange}
-          />
-        </Col>
-        <Col>
-          <Form.Label htmlFor="inputSquare">Square</Form.Label>
-          <Form.Control
-            type="text"
-            id="inputSquare"
-            name="square"
-            value={formData.square}
-            onChange={handleInputChange}
-          />
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <Form.Group controlId="img" className="mb-3">
-            <Form.Label>Upload photo 1</Form.Label>
+    <>
+      <button onClick={handleReturnBackClick}>return to the dashboard</button>
+
+      <Form onSubmit={handleSubmit}>
+        <Row>
+          <Col>
+            <Form.Select
+              aria-label="Default select example"
+              name="sale"
+              value={formData.sale}
+              onChange={handleInputChange}
+            >
+              <option value="sale">Sale</option>
+              <option value="rent">Rent</option>
+            </Form.Select>
+          </Col>
+          <Col>
+            <Form.Select
+              aria-label="Default select example"
+              name="property"
+              value={formData.property}
+              onChange={handleInputChange}
+            >
+              <option value="offices">Office</option>
+              <option value="shops">Shop</option>
+              <option value="warehouses">Warehouse</option>
+              <option value="catering">Catering</option>
+            </Form.Select>
+          </Col>
+          <Col>
+            <Form.Select
+              aria-label="Default select example"
+              name="place"
+              value={formData.place}
+              onChange={handleInputChange}
+            >
+              <option value="Zvezdara">Zvezdara</option>
+              <option value="Novi Beograd">Novi Beograd</option>
+              <option value="Savski venac">Savski venac</option>
+              <option value="Palilula">Palilula</option>
+              <option value="Stari grad">Stari grad</option>
+              <option value="Mladenovac">Mladenovac</option>
+            </Form.Select>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Form.Label htmlFor="inputPrice">Price</Form.Label>
             <Form.Control
-              type="file"
-              ref={file1Ref}
-              onChange={handleFileChange}
+              type="text"
+              id="inputPrice"
+              name="price"
+              value={formData.price}
+              onChange={handleInputChange}
             />
-          </Form.Group>
-        </Col>
-        <Col>
-          <Form.Group controlId="img1" className="mb-3">
-            <Form.Label>upload photo 2</Form.Label>
+          </Col>
+          <Col>
+            <Form.Label htmlFor="inputSquare">Square</Form.Label>
             <Form.Control
-              type="file"
-              ref={file2Ref}
-              onChange={handleFileChange}
+              type="text"
+              id="inputSquare"
+              name="square"
+              value={formData.square}
+              onChange={handleInputChange}
             />
-          </Form.Group>
-        </Col>
-        <Col>
-          <Form.Group controlId="img2" className="mb-3">
-            <Form.Label>Upload photo 3</Form.Label>
-            <Form.Control
-              type="file"
-              ref={file3Ref}
-              onChange={handleFileChange}
-            />
-          </Form.Group>
-        </Col>
-        <Col>
-          <Form.Group controlId="img3" className="mb-3">
-            <Form.Label>Upload photo 4</Form.Label>
-            <Form.Control
-              type="file"
-              ref={file4Ref}
-              onChange={handleFileChange}
-            />
-          </Form.Group>
-        </Col>
-      </Row>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Form.Group controlId="img" className="mb-3">
+              <Form.Label>Upload photo 1</Form.Label>
+              <Form.Control
+                type="file"
+                ref={file1Ref}
+                onChange={handleFileChange}
+              />
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group controlId="img1" className="mb-3">
+              <Form.Label>upload photo 2</Form.Label>
+              <Form.Control
+                type="file"
+                ref={file2Ref}
+                onChange={handleFileChange}
+              />
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group controlId="img2" className="mb-3">
+              <Form.Label>Upload photo 3</Form.Label>
+              <Form.Control
+                type="file"
+                ref={file3Ref}
+                onChange={handleFileChange}
+              />
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group controlId="img3" className="mb-3">
+              <Form.Label>Upload photo 4</Form.Label>
+              <Form.Control
+                type="file"
+                ref={file4Ref}
+                onChange={handleFileChange}
+              />
+            </Form.Group>
+          </Col>
+        </Row>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </>
   );
 };
 
