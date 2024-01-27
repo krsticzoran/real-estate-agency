ce88bcc8302d54d2c7e77d8c9d5650159f71b584
zorannkrstic <zorankrstic81@gmail.com>
2024-01-27 11:36:51 +0100

    back to dashboard

--- src/components/dashboard/BackToTheDashboard.tsx
+++ src/components/dashboard/BackToTheDashboard.tsx
@@ -1,12 +1,33 @@
 import { FC } from "react";
-import { useNavigate } from "react-router";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 
 const BackToTheDashboard: FC = () => {
-  const navigate = useNavigate();
-  const handleReturnBackClick = () => {
-    navigate("/dashboard/");
+  const buttonStyle = {
+    margin: "15px",
+    textDecoration: "none",
+    padding: "10px 15px",
+    backgroundColor: "#5c0282",
+    color: "#fff",
+    borderRadius: "5px",
+    display: "flex",
+    alignItems: "center",
+    cursor: "pointer",
+    transition: "background-color 0.3s ease",
+    width: "fit-content",
   };
-  return <button onClick={handleReturnBackClick}>Back to the dashboard</button>;
+
+  const iconStyle = {
+    marginRight: "5px",
+  };
+
+  return (
+    <Link to="/dashboard" style={buttonStyle}>
+      <FontAwesomeIcon icon={faArrowLeft} style={iconStyle} />
+      Back to Dashboard
+    </Link>
+  );
 };
 
 export default BackToTheDashboard;
