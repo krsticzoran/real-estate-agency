1db95d9286fb483e23cab437ba3b64ea90eb24eb
zorannkrstic <zorankrstic81@gmail.com>
2024-02-03 18:13:15 +0100

    header mobile style

--- src/pages/dashboard/header/Header.tsx
+++ src/pages/dashboard/header/Header.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import admin from "../../../assets/images/dashboard/admin.jpeg";
 import Form from "react-bootstrap/Form";
+import { useMediaQuery } from "react-responsive";
 
 interface HeaderProps {
   isChecked: boolean;
@@ -8,26 +9,33 @@ interface HeaderProps {
 }
 
 const Header: React.FC<HeaderProps> = ({ isChecked, handleSwitchChange }) => {
+  const isMobile = useMediaQuery({ maxWidth: 992 });
   return (
-    <div className=" d-flex justify-content-between align-items-center dashboard-header ">
-      <p className="pt-2">Welcome to RE Belgrade dashboard</p>
-      <div className="d-flex align-items-center">
-        <Form className="p-5 ">
-          <Form.Check
-            type="switch"
-            id="custom-switch"
-            label={isChecked ? "Dark" : "Light"}
-            checked={isChecked}
-            onChange={handleSwitchChange}
-          />
-        </Form>
-        <div className="dash-header-info">
-          <span>Henry Jr.</span>
-          <p>Admin</p>
+    <>
+      {isMobile ? (
+        ""
+      ) : (
+        <div className=" d-flex justify-content-between align-items-center dashboard-header ">
+          <p className="pt-2">Welcome to RE Belgrade dashboard</p>
+          <div className="d-flex align-items-center">
+            <Form className="p-5 ">
+              <Form.Check
+                type="switch"
+                id="custom-switch"
+                label={isChecked ? "Dark" : "Light"}
+                checked={isChecked}
+                onChange={handleSwitchChange}
+              />
+            </Form>
+            <div className="dash-header-info">
+              <span>Henry Jr.</span>
+              <p>Admin</p>
+            </div>
+            <img src={admin} alt="admin" />
+          </div>
         </div>
-        <img src={admin} alt="admin" />
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
