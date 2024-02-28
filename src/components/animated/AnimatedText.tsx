2185f15708cfb6e35f0eac6972b1b5e4fe041472
zorannkrstic <zorankrstic81@gmail.com>
2024-02-28 18:15:30 +0100

    animated text

---
+++ src/components/animated/AnimatedText.tsx
@@ -0,0 +1,54 @@
+import { FC } from "react";
+import { motion } from "framer-motion";
+
+interface AnimatedTextProps {
+  text: string;
+}
+
+const AnimatedText: FC<AnimatedTextProps> = ({ text }) => {
+  const letters = Array.from(text);
+
+  const container = {
+    hidden: { opacity: 0 },
+    visible: (i = 1) => ({
+      opacity: 1,
+      transition: { staggerChildren: 0.04, delayChildren: 0.4 * i },
+    }),
+  };
+
+  const child = {
+    visible: {
+      opacity: 1,
+      transition: {
+        type: "spring",
+        damping: 12,
+        stiffness: 10,
+      },
+    },
+    hidden: {
+      opacity: 0,
+      transition: {
+        type: "spring",
+        damping: 12,
+        stiffness: 100,
+      },
+    },
+  };
+
+  return (
+    <motion.div
+      style={{ overflow: "hidden", display: "flex" }}
+      variants={container}
+      initial="hidden"
+      animate="visible"
+    >
+      {letters.map((letter, index) => (
+        <motion.span key={index} variants={child}>
+          {letter === " " ? "\u00A0" : letter}
+        </motion.span>
+      ))}
+    </motion.div>
+  );
+};
+
+export default AnimatedText;
--- src/pages/home/property-box/CommercialSpecialists.tsx
+++ src/pages/home/property-box/CommercialSpecialists.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import "./property-box.css";
+import AnimatedText from "../../../components/animated/AnimatedText";
 
 const CommercialSpecialists: React.FC = () => {
   return (
     <div className="col-md-6 col-sm-12 col-xs-12 d-flex align-content-sm-start align-content-md-center  flex-wrap">
       <div>
-        <h4 className="commercial-h4-text ">COMMERCIAL SPECIALISTS</h4>
+        <h4 className="commercial-h4-text ">
+          <AnimatedText text="COMMERCIAL SPECIALISTS" />
+        </h4>
         <h1 className="commercial-h1-text">
-          {"Find your next"}
-          <br />
-          {"commercial property"}
+          <AnimatedText text="Find your next" />
+        </h1>
+        <h1 className="commercial-h1-text">
+          <AnimatedText text="commercial property" />
         </h1>
       </div>
     </div>
