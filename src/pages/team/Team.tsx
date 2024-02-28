e3861937033895561b6f7e43512eeeeaa7436c30
zorannkrstic <zorankrstic81@gmail.com>
2024-02-28 19:02:25 +0100

    animated text and list

--- src/pages/team/Team.tsx
+++ src/pages/team/Team.tsx
@@ -8,6 +8,9 @@ import "./team.css";
 import { useQuery } from "@apollo/client";
 import { gql } from "graphql-tag";
 
+import AnimatedComponentList from "../../components/animated/AnimatedComponentList";
+import AnimatedText from "../../components/animated/AnimatedText";
+
 interface User {
   user: string;
   name: string;
@@ -42,25 +45,31 @@ const Team: FC = () => {
   return (
     <div className="team">
       <Container>
-        <h2 className="team-title">Team</h2>
-        <h4>Our Specialist</h4>
+        <h2 className="team-title">
+          <AnimatedText text="Team" />
+        </h2>
+        <h4>
+          <AnimatedText text="Our Specialist" />
+        </h4>
         <Row>
-          {staff.map((member) => (
+          {staff.map((member, index) => (
             <div key={member.user} className="col-lg-4 col-sm-12 team-card">
               <Link to={`/team/${member.user}`}>
-                <Card>
-                  <Card.Body>
-                    <img src={member.img} alt="staff" />
-                    <div>
-                      <h5 className="team-card-title">{member.name}</h5>
-                      <p>
-                        <strong>Speaks:</strong> {member.language}
-                        <br />
-                        <strong>Email:</strong> {member.email}
-                      </p>
-                    </div>
-                  </Card.Body>
-                </Card>
+                <AnimatedComponentList index={index}>
+                  <Card>
+                    <Card.Body>
+                      <img src={member.img} alt="staff" />
+                      <div>
+                        <h5 className="team-card-title">{member.name}</h5>
+                        <p>
+                          <strong>Speaks:</strong> {member.language}
+                          <br />
+                          <strong>Email:</strong> {member.email}
+                        </p>
+                      </div>
+                    </Card.Body>
+                  </Card>
+                </AnimatedComponentList>
               </Link>
             </div>
           ))}
