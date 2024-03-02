5228df7ef571f2cd0f438c789c296ef07f78eca6
zorannkrstic <zorankrstic81@gmail.com>
2024-03-02 13:22:02 +0100

    animated hover effect

--- src/pages/team/Team.tsx
+++ src/pages/team/Team.tsx
@@ -10,6 +10,7 @@ import { gql } from "graphql-tag";
 
 import AnimatedComponentList from "../../components/animated/AnimatedComponentList";
 import AnimatedText from "../../components/animated/AnimatedText";
+import AnimatedHoverCard from "../../components/animated/AnimatedHoverCard";
 
 interface User {
   user: string;
@@ -56,19 +57,21 @@ const Team: FC = () => {
             <div key={member.user} className="col-lg-4 col-sm-12 team-card">
               <Link to={`/team/${member.user}`}>
                 <AnimatedComponentList index={index}>
-                  <Card>
-                    <Card.Body>
-                      <img src={member.img} alt="staff" />
-                      <div>
-                        <h5 className="team-card-title">{member.name}</h5>
-                        <p>
-                          <strong>Speaks:</strong> {member.language}
-                          <br />
-                          <strong>Email:</strong> {member.email}
-                        </p>
-                      </div>
-                    </Card.Body>
-                  </Card>
+                  <AnimatedHoverCard>
+                    <Card>
+                      <Card.Body>
+                        <img src={member.img} alt="staff" />
+                        <div>
+                          <h5 className="team-card-title">{member.name}</h5>
+                          <p>
+                            <strong>Speaks:</strong> {member.language}
+                            <br />
+                            <strong>Email:</strong> {member.email}
+                          </p>
+                        </div>
+                      </Card.Body>
+                    </Card>
+                  </AnimatedHoverCard>
                 </AnimatedComponentList>
               </Link>
             </div>
