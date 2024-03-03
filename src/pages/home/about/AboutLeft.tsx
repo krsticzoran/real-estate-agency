9dba93310e52d70a6802b1d5b765c3b191575a9d
zorannkrstic <zorankrstic81@gmail.com>
2024-03-03 10:32:24 +0100

    about animated

--- src/pages/home/about/AboutLeft.tsx
+++ src/pages/home/about/AboutLeft.tsx
@@ -1,22 +1,27 @@
 import { FC } from "react";
 import { Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import AnimatedComponentList from "../../../components/animated/AnimatedComponentList";
 
 const AboutLeft: FC = () => {
   return (
     <div className="col-md-5 col-sm-12">
-      <h4 className="about-us">ABOUT US</h4>
-      <h2>Transforming the Commercial Landscape of the City!</h2>
-      <p className="about-left-paragraph">
-        We strive to provide our clients with the best possible experience when
-        starting up or relocating your business. With an extensive database
-        dedicated to commercial properties that is updated daily, our team of
-        extensively trained commercial specialists, we are the best choice for
-        your business!
-      </p>
-      <Link className="navbar--color-white nav-link" to="/about-us">
-        <Button className="btn btn-about">Read more..</Button>
-      </Link>
+      <AnimatedComponentList index={1}>
+        <h4 className="about-us">ABOUT US</h4>
+        <h2>Transforming the Commercial Landscape of the City!</h2>
+        <p className="about-left-paragraph">
+          We strive to provide our clients with the best possible experience
+          when starting up or relocating your business. With an extensive
+          database dedicated to commercial properties that is updated daily, our
+          team of extensively trained commercial specialists, we are the best
+          choice for your business!
+        </p>
+      </AnimatedComponentList>
+      <AnimatedComponentList index={2}>
+        <Link className="navbar--color-white nav-link" to="/about-us">
+          <Button className="btn btn-about">Read more..</Button>
+        </Link>
+      </AnimatedComponentList>
     </div>
   );
 };
--- src/pages/home/about/AboutRight.tsx
+++ src/pages/home/about/AboutRight.tsx
@@ -2,61 +2,66 @@ import { FC } from "react";
 import img1 from "../../../assets/images/home/benefit01.png";
 import img2 from "../../../assets/images/home/benefit02.png";
 import img3 from "../../../assets/images/home/benefit03.png";
+import AnimatedComponentList from "../../../components/animated/AnimatedComponentList";
 
 const AboutRight: FC = () => {
   return (
     <div className="col-md-7 col-sm-12 about-right-box">
-      <div className="right-box">
-        <div className="right-item ">
-          <div className="right-img-box">
-            <img src={img1} alt="benefit" className="right-img" />
+      <AnimatedComponentList index={2}>
+        <div className="right-box">
+          <div className="right-item ">
+            <div className="right-img-box">
+              <img src={img1} alt="benefit" className="right-img" />
+            </div>
+            <div className="right-text-box">
+              <h3 className="right-title">Local Knowledge</h3>
+              <p className="right-text mt-1">
+                Led by Locals who love Belgrade and what they do.
+              </p>
+            </div>
           </div>
-          <div className="right-text-box">
-            <h3 className="right-title">Local Knowledge</h3>
-            <p className="right-text mt-1">
-              Led by Locals who love Belgrade and what they do.
-            </p>
-          </div>
-        </div>
-        <div className="right-item">
-          <div className="right-img-box">
-            <img src={img2} alt="benefit" className="right-img" />
-          </div>
-          <div className="right-text-box">
-            <h3 className="right-title">Expert Team</h3>
-            <p className="right-text mt-1">
-              Our team of Commercial Specialists are exstensively trained and
-              given the best resources to help you on your search.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div className="right-box mt-md-5 mt-md-0">
-        <div className="right-item ">
-          <div className="right-img-box">
-            <img src={img3} alt="benefit" className="right-img" />
-          </div>
-          <div className="right-text-box">
-            <h3 className="right-title">Specialising in Commercial Property</h3>
-            <p className="right-text mt-1">
-              We specialize in all types of commercial properties ranging from
-              offices, retail, industry, hospitality, catering & more.
-            </p>
+          <div className="right-item">
+            <div className="right-img-box">
+              <img src={img2} alt="benefit" className="right-img" />
+            </div>
+            <div className="right-text-box">
+              <h3 className="right-title">Expert Team</h3>
+              <p className="right-text mt-1">
+                Our team of Commercial Specialists are exstensively trained and
+                given the best resources to help you on your search.
+              </p>
+            </div>
           </div>
         </div>
-        <div className="right-item">
-          <div className="right-img-box">
-            <img src={img1} alt="benefit" className="right-img" />
+        <div className="right-box mt-md-5 mt-md-0">
+          <div className="right-item ">
+            <div className="right-img-box">
+              <img src={img3} alt="benefit" className="right-img" />
+            </div>
+            <div className="right-text-box">
+              <h3 className="right-title">
+                Specialising in Commercial Property
+              </h3>
+              <p className="right-text mt-1">
+                We specialize in all types of commercial properties ranging from
+                offices, retail, industry, hospitality, catering & more.
+              </p>
+            </div>
           </div>
-          <div className="right-text-box">
-            <h3 className="right-title">Trusted Partners</h3>
-            <p className="right-text mt-1">
-              Trusted by many local and international businesses in finding
-              their locations.
-            </p>
+          <div className="right-item">
+            <div className="right-img-box">
+              <img src={img1} alt="benefit" className="right-img" />
+            </div>
+            <div className="right-text-box">
+              <h3 className="right-title">Trusted Partners</h3>
+              <p className="right-text mt-1">
+                Trusted by many local and international businesses in finding
+                their locations.
+              </p>
+            </div>
           </div>
         </div>
-      </div>
+      </AnimatedComponentList>
     </div>
   );
 };
