62027bac912c9d15800eb195236b934aeb8fe1c1
zorannkrstic <zorankrstic81@gmail.com>
2024-04-06 12:53:43 +0200

    handle scroll to top

--- src/pages/home/backToTopButton/BackToTopButton.tsx
+++ src/pages/home/backToTopButton/BackToTopButton.tsx
@@ -3,10 +3,14 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowUp } from "@fortawesome/free-solid-svg-icons";
 import "./backToTopButton.css";
 
+const handleScrollToTop = () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+};
+
 const BackToTopButton: FC = () => {
   return (
     <div className="backToTopButtonContainer">
-      <button className="backToTopButton">
+      <button className="backToTopButton" onClick={handleScrollToTop}>
         <FontAwesomeIcon icon={faArrowUp} />
       </button>
     </div>
